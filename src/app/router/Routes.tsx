2641a35c6c48071b783e90340bfd7a1c896c8193
alishokoohi23 <alibest3@yahoo.com>
2024-11-01 14:20:46 +0330

    feat(verifiication): add Verification feature

--- src/app/router/Routes.tsx
+++ src/app/router/Routes.tsx
@@ -1,6 +1,7 @@
 import { RouteObject, Navigate, createBrowserRouter } from "react-router-dom";
 import App from "../../App";
 import ForgetPassword from "../../features/user/forgetPassword/ForgetPassword";
+import Verification from "../../features/user/verification/Verification";
 
 export const routes: RouteObject[] = [
     {
@@ -12,6 +13,7 @@ export const routes: RouteObject[] = [
       //  {path:"/login",  element: <Login />},
       //  {path:"/register",  element: <Register />},
        {path:"/forget-password",  element:<ForgetPassword />},
+       {path:"/verify",  element:<Verification />},
       //  {path:"not-found",  element: <PageNotFound />},
        { path: "*", element: <Navigate replace to="not-found" /> },
       ],
