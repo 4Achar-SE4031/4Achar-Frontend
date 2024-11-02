40deab33854bb706a798345595b30fa65c0f6f17
Sajjad <13.sajjad.mehrpeyma.81@gmail.com>
2024-11-02 20:47:54 +0330

    feat(Register): css files added

--- src/app/router/Routes.tsx
+++ src/app/router/Routes.tsx
@@ -1,5 +1,6 @@
 import { RouteObject, Navigate, createBrowserRouter } from "react-router-dom";
-import App from "../App";
+import App from "../../App";
+import Register from "../../features/user/register/Register";
 
 export const routes: RouteObject[] = [
     {
@@ -9,7 +10,7 @@ export const routes: RouteObject[] = [
       //  {path:"/" , element: <Landing />},
       //  {path:"/home",  element: <Home />},
       //  {path:"/login",  element: <Login />},
-      //  {path:"/register",  element: <Register />},
+       {path:"/register",  element: <Register />},
       //  {path:"/password-recovery",  element:<PasswordRecovery />},
       //  {path:"not-found",  element: <PageNotFound />},
        { path: "*", element: <Navigate replace to="not-found" /> },
