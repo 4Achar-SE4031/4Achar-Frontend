d249de7431e51fcf3802d33a15acdb1de51fdcf4
alishokoohi23 <alibest3@yahoo.com>
2024-11-27 20:55:01 +0330

    feat(EvensList): design new card

--- src/features/events/EventItem.tsx
+++ src/features/events/EventItem.tsx
@@ -2,6 +2,9 @@ import React from "react";
 import { Link } from "react-router-dom";
 import photo1 from "../../assets/Images/events.jpg";
 import { Event } from "../../app/models/event";
+import { observer } from "mobx-react-lite";
+import toPersianDigits from "../../app/common/toPersianDigits";
+
 
 interface EventItemProps {
   event: Event;
@@ -9,9 +12,8 @@ interface EventItemProps {
 
 const EventItem: React.FC<EventItemProps> = ({ event }) => {
   return (
-    <div className="item mb-3">
-      {/* <Link to={`/concertdetail/${event.id}`}></Link> */}
-      <Link to={`/concertdetail`}>
+    <Link to={`/concertdetail`}>
+      <div className="item mb-3">
         <div className="event-img">
           <img
             alt={event.title}
@@ -19,20 +21,31 @@ const EventItem: React.FC<EventItemProps> = ({ event }) => {
             onError={(e) => (e.currentTarget.src = photo1)}
           />
         </div>
-      </Link>
-      
-      {/* <Link to={`/concertdetail/${event.id}`}></Link> */}
-      <Link to={`/concertdetail`}>
+
         <div className="event-info">
-          <h1>{event.title}</h1>
-          <h4>تاریخ: {event.start_date}</h4>
-          {/* {event.attendance === "O" ? <h4>آنلاین</h4> : <h4>{`${event.province} ${event.city}`}</h4>} */}
-          {/* <h4>{event.category}</h4> */}
-          <h5>{event.is_paid ? `${event.ticket_price} تومان` : "رایگان"}</h5>
+          <div className="event-info__title">
+            <h1 id="event-title">{event.title} نمایش امشب</h1>
+          </div>
+          <div className="event-info__details">
+            <h4 id="event-details">{event.details} آخرین دوره اجراها در ایران</h4>
+            <i className="input-icon uil uil-document-layout-right"></i>
+          </div>
+          <div className="event-info__address"> 
+            <h4 id="event-address">{`${event.province} ${event.city}`}کاخ هنر </h4>
+            <i className="input-icon uil uil-location-point"></i>
+          </div>
+          <div className="event-info__date">
+            <h4 id="event-date">{toPersianDigits(event.start_date)}</h4>
+            <i className="input-icon uil uil-calendar-alt"></i>
+          </div>
+          <div className="event-info__hour">
+            <h5 id="event-hour">{toPersianDigits(event.start_hour)} {toPersianDigits("19:00")}</h5>
+            <i className="input-icon uil uil-clock"></i>
+          </div>
         </div>
-      </Link>
-    </div>
+      </div>
+    </Link>
   );
 };
 
-export default EventItem;
+export default observer(EventItem);
