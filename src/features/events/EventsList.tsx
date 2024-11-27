73a9e1293159862f66c4f53c59e3e0220dba66e7
alishokoohi23 <alibest3@yahoo.com>
2024-11-27 20:58:07 +0330

    fix(EventsList): add slider to Home Page

--- src/features/events/EventsList.tsx
+++ src/features/events/EventsList.tsx
@@ -1,27 +1,32 @@
 import React, { useState, useEffect } from "react";
-import { Event } from "../../app/models/event";
-import agent from "../../app/api/agent";
 import Card from "../../app/common/Card/Card";
-import Pagination from "@mui/material/Pagination";
+// import Pagination from "@mui/material/Pagination";
 import Stack from "@mui/material/Stack";
 import Lottie from "react-lottie";
+import { observer } from "mobx-react-lite";
+
+import { Event } from "../../app/models/event";
+import agent from "../../app/api/agent";
 import animationData from "../../app/common/lottie/Animation - 1715854965467.json";
 import "./EventsList.css";
 import EventItem from "./EventItem";
 import mockEvents from "../../app/common/Mock Data/MOCK_DATA.json";
+import { useStore } from "../../app/store/Store";
+import { useLocation } from "react-router-dom";
+import Pagination from "../../app/common/Pagination";
 
 
-interface EventsListProps {
-  sortType: "recent" | "popular";
-}
-
-const EventsList: React.FC<EventsListProps> = ({ sortType }) => {
+const EventsList: React.FC = () => {
   const [posts, setPosts] = useState<Event[]>([]);
   const [loading, setLoading] = useState<boolean>(true);
   const [currentPage, setCurrentPage] = useState<number>(1);
   const [totalPages, setTotalPages] = useState<number>(1);
   const [index, setIndex] = useState<number>(1);
   const [title, setTitle] = useState<string>();
+  const { eventStore } = useStore()
+  const { eventType, setEventType } = eventStore
+  // const [sortType, setSortType] = useState<string | undefined>(eventType)
+  const location = useLocation()
 
 
   const defaultOptions = {
@@ -49,19 +54,25 @@ const EventsList: React.FC<EventsListProps> = ({ sortType }) => {
   useEffect(() => {
     setLoading(true);
     // Mock data handling instead of API call
+    // setSortType(eventType)
+    // console.log(sortType)
     let sortedEvents = [...mockEvents];
-    if (sortType === "recent") {
+    if (!eventType){
+      setEventType(location.pathname.split("/")[2])
+    }
+    if (eventType === "recent") {
       sortedEvents = sortedEvents.sort((a, b) => Date.parse(b.start_date) - Date.parse(a.start_date));
       setTitle("جدیدترین رویدادها")
-    } else if (sortType === "popular") {
+    } else if (eventType === "popular") {
       sortedEvents = sortedEvents.sort((a, b) => b.rating - a.rating);
       setTitle("محبوب ترین رویدادها")
+      
     }
 
     setPosts(sortedEvents.slice(15 * (index - 1), 15 * index));
     setTotalPages(Math.ceil(sortedEvents.length / 15));
     setLoading(false);
-  }, [sortType, currentPage]);
+  }, [eventType, currentPage]);
 
   const handleChangePage = (_event: React.ChangeEvent<unknown>, value: number) => {
     setCurrentPage(value);
@@ -90,7 +101,7 @@ const EventsList: React.FC<EventsListProps> = ({ sortType }) => {
             </div>
           ))}
           <Stack spacing={2}>
-            <Pagination count={totalPages} color="primary" onChange={handleChangePage} />
+            <Pagination count={totalPages} page={currentPage} onChange={handleChangePage} />
           </Stack>
         </div>
       </div>
@@ -98,4 +109,4 @@ const EventsList: React.FC<EventsListProps> = ({ sortType }) => {
   );
 };
 
-export default EventsList;
+export default observer(EventsList);
--- src/features/events/FiveEvents.tsx
+++ src/features/events/FiveEvents.tsx
@@ -1,34 +1,67 @@
 import React, { useState, useEffect } from "react";
-import { Event } from "../../app/models/event";
-import agent from "../../app/api/agent";
-import Card from "../../app/common/Card/Card";
 import Pagination from "@mui/material/Pagination";
 import Stack from "@mui/material/Stack";
 import Lottie from "react-lottie";
+import { observer } from "mobx-react-lite";
+
+import { Event } from "../../app/models/event";
+import EventItem from "./EventItem";
+import Card from "../../app/common/Card/Card";
+import agent from "../../app/api/agent";
 import animationData from "../../app/common/lottie/Animation - 1715854965467.json";
 import "./EventsList.css";
-import EventItem from "./EventItem";
 import mockEvents from "../../app/common/Mock Data/MOCK_DATA.json";
+import { useStore } from "../../app/store/Store";
+import { useNavigate } from "react-router-dom";
 
 const FiveEvents: React.FC = () => {
   const [posts, setPosts] = useState<Event[]>([]);
   const [loading, setLoading] = useState<boolean>(true);
   const [currentPage, setCurrentPage] = useState<number>(1);
   const [totalPages, setTotalPages] = useState<number>(1);
+  const { eventStore } = useStore()
+  const [recentEventIndex, setRecentEventIndex] = useState(0);
+  const [popularEventIndex, setPopularEventIndex] = useState(0);
+  const eventsToShow = 5;
+  const { eventType, showType, setEventType } = eventStore;
+  const navigate = useNavigate();
 
-  const recentEvents = [...mockEvents].sort((a, b) => Date.parse(b.start_date) - Date.parse(a.start_date)).slice(0, 5);
-  const popularEvents = [...mockEvents].sort((a, b) => b.rating - a.rating).slice(0, 5);
+  const recentEvents = [...mockEvents]
+    .sort((a, b) => Date.parse(b.start_date) - Date.parse(a.start_date))
+    .slice(recentEventIndex, recentEventIndex + eventsToShow);
 
+  const popularEvents = [...mockEvents]
+    .sort((a, b) => b.rating - a.rating)
+    .slice(popularEventIndex, popularEventIndex + eventsToShow);
   const defaultOptions = {
     loop: true,
     autoplay: true,
     animationData,
   };
 
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setRecentEventIndex((prevIndex) =>
+        prevIndex + 1 >= mockEvents.length - eventsToShow ? 0 : prevIndex + 1
+      );
+      setPopularEventIndex((prevIndex) =>
+        prevIndex + 1 >= mockEvents.length - eventsToShow ? 0 : prevIndex + 1
+      );
+    }, 5000);
+
+    return () => clearInterval(timer);
+  }, []);
+
 
   const handleChangePage = (_event: React.ChangeEvent<unknown>, value: number) => {
     setCurrentPage(value);
   };
+
+  const handleShowTypeButton = (value: string) => {
+    setEventType(value)
+    console.log(value)
+    navigate(`/events/${value}`)
+  }
 
   return (
     <Card className="events-list">
@@ -36,7 +69,7 @@ const FiveEvents: React.FC = () => {
         <div className="d-flex justify-content-between align-items-center mb-2 mt-4">
           <button
             className="btn btn-primary show-all"
-            onClick={() => window.location.href = '/events/recent'}
+            onClick={() => handleShowTypeButton("recent")}
           >
             نمایش همه
           </button>
@@ -44,25 +77,45 @@ const FiveEvents: React.FC = () => {
             رویدادهای جدید
           </h2>
         </div>
-
-        <div className="items">
-          {/* {loading && (
+        <div className="position-relative">
+          <button
+            className="slider-arrow left"
+            onClick={() => setRecentEventIndex(prev =>
+              prev === 0 ? mockEvents.length - eventsToShow : prev - 1
+            )}
+          >
+            &#8249;
+          </button>
+          <div className="items">
+            {/* {loading && (
             <div className="loading">
               <Lottie options={defaultOptions} />
             </div>
           )} */}
-          {recentEvents.map(event => (
-            <div key={event.id} className="col-lg-2 col-md-3 col-sm-5 mb-3">
-              <EventItem event={event} />
-            </div>
-          ))}
+            {recentEvents.map(event => (
+              <div key={event.id} className="col-lg-2 col-md-3 col-sm-5 mb-3">
+                <EventItem event={event} />
+              </div>
+            ))}
+
+            <button
+              className="slider-arrow right"
+              onClick={() => setRecentEventIndex(prev =>
+                prev + 1 >= mockEvents.length - eventsToShow ? 0 : prev + 1
+              )}
+            >
+              &#8250;
+            </button>
+          </div>
+
         </div>
 
 
         <div className="d-flex justify-content-between align-items-center mb-2 mt-4">
           <button
             className="btn btn-primary show-all"
-            onClick={() => window.location.href = '/events/recent'}
+            // onClick={() => window.location.href = '/events/popular'}
+            onClick={() => handleShowTypeButton("popular")}
           >
             نمایش همه
           </button>
@@ -70,22 +123,40 @@ const FiveEvents: React.FC = () => {
             رویدادهای محبوب
           </h2>
         </div>
-        <div className="items">
-          {/* {loading && (
-            <div className="loading">
+
+        <div className="position-relative">
+          <button
+            className="slider-arrow left"
+            onClick={() => setPopularEventIndex(prev =>
+              prev === 0 ? mockEvents.length - eventsToShow : prev - 1
+            )}
+          >
+            &#8249;
+          </button>
+          <div className="items">
+            {/* {loading && (
+            <div className="loading"> 
               <Lottie options={defaultOptions} />
             </div>
           )} */}
-          {popularEvents.map(event => (
-            <div key={event.id} className="col-md-2 col-sm-6 mb-3">
-              <EventItem event={event} />
-            </div>
-          ))}
+            {popularEvents.map(event => (
+              <div key={event.id} className="col-md-2 col-sm-6 mb-3">
+                <EventItem event={event} />
+              </div>
+            ))}
+          </div>
+          <button
+            className="slider-arrow right"
+            onClick={() => setPopularEventIndex(prev =>
+              prev + 1 >= mockEvents.length - eventsToShow ? 0 : prev + 1
+            )}
+          >
+            &#8250;
+          </button>
         </div>
-
       </div>
     </Card>
   );
 };
 
-export default FiveEvents;
+export default observer(FiveEvents);
