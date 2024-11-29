552fa1b3b16c9d8dfd5c2502e98e023f4b5e9e3d
alishokoohi23 <alibest3@yahoo.com>
2024-11-29 21:41:09 +0330

    fix(comment): fix font

--- src/features/Comment/MainComment.tsx
+++ src/features/Comment/MainComment.tsx
@@ -134,7 +134,7 @@ const MainComment: React.FC<MainCommentProps> = ({ id }) => {
   };
 
   return (
-    <div className="container-fluid mb-5 pb-5">
+    <div className="container-fluid mb-5 pb-5" lang="fa">
       <div className="row">
         <div className="col-6 mx-auto">
           <div className="comment-section-title text-center">
