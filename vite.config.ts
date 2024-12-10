193e1f896889434ffab8dfc06999d16f738759d4
Sajjad <13.sajjad.mehrpeyma.81@gmail.com>
2024-12-10 23:49:24 +0330

    fix(Develop): added Dockerfile

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,20 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   server: {
+    host: true,
     port: 3000
   },
   plugins: [react()],
+  build: {
+    // This will handle large font files or other assets
+    assetsInlineLimit: 0, // Ensure all assets are copied instead of inlined
+    chunkSizeWarningLimit: 2500, // Increase the chunk size warning limit to 1 MB (1000 KB)
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom', 'lottie-web'], // Example of manual chunking
+        },
+      },
+    },
+  },
 })
