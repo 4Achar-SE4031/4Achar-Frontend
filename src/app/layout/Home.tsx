c15cfb819c46ca83d0f116dd8abdd28d439f7207
alishokoohi23 <alibest3@yahoo.com>
2024-12-12 22:58:58 +0330

    feat(Hero): add Hero section on landing page

--- src/app/layout/Home.tsx
+++ src/app/layout/Home.tsx
@@ -1,10 +1,12 @@
 import { observer } from 'mobx-react-lite'
 import FiveEvents from '../../features/events/FiveEvents'
 import FooterComponent from './Footer'
+import HeroSection from "./HeroSection"
 
 const Home = () => {
   return (
-    <div>
+    <div id="mainScrollContainer">
+      <HeroSection />
       <FiveEvents />
       <FooterComponent />
     </div>
