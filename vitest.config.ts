af1dad124c73a88fa09363685a6df16e40aff7b4
Sajjad <13.sajjad.mehrpeyma.81@gmail.com>
2024-12-26 18:55:06 +0330

    feat(loginTest): login/signup test completed

---
+++ vitest.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vitest/config'
+
+export default defineConfig({
+    // plugins: [react()],
+    test: {
+        globals: true,
+        environment: 'jsdom',
+    coverage: {
+    //   provider: 'v8', // 'c8' is the default, but you can specify it explicitly
+      reporter: ['text', 'html'], // You can choose the format of the coverage report (text, html, etc.)
+      include: ['**/*.test.tsx'], // Files to include for coverage (usually your src folder)
+      exclude: ['node_modules', 'tests'], // Files to exclude from coverage (usually node_modules and test files)
+    },
+  },
+})
