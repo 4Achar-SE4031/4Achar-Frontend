fd23d924e14682a7da2492876711553b775e474b
alishokoohi23 <alibest3@yahoo.com>
2024-12-28 12:58:34 +0330

    fix(Hero): fix test

--- setup.ts
+++ setup.ts
@@ -1,6 +1,7 @@
-import '@testing-library/jest-dom'; // Adds matchers like toBeInTheDocument().
+import '@testing-library/jest-dom'; 
 import { vi } from 'vitest';
 import "vitest-canvas-mock"
+
 
 // Mock any global modules or APIs as needed.
 global.fetch = vi.fn();
